6b41509b15f8f2dee5d2107016762c755f2fe4ea
Scott <whatsr00t@gmail.com>
2020-07-24 14:15:29 -0600

    initial skeleton

---
+++ flight_review_backend/src/main/java/com/galvanize/flight_review_backend/FlightReviewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.flight_review_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightReviewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightReviewBackendApplication.class, args);
+	}
+
+}
---
+++ flight_review_backend/src/test/java/com/galvanize/flight_review_backend/FlightReviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.flight_review_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightReviewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
